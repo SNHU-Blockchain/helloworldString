46d51c605c67d1a4ebfd6aee440d21a5eacac55c
ladeeluc <laura.hatch@snhu.edu>
2024-05-17 19:25:33 -0500

    completed math part

--- Blockchain.py/arithmetic_operations.py
+++ Blockchain.py/arithmetic_operations.py
@@ -1,1 +1,32 @@
 def addNum():
+    sum = 0
+    num_a = 2
+    num_b = 2
+
+    sum = num_a + num_b
+    print("These are numbers added together: " + str(sum))
+addNum()
+
+def subNum():
+    num_c = 4
+    num_d = 2
+
+    dif = num_c - num_d
+    print ("These are numbers subtracted: " + str(dif))
+subNum()
+
+def multiNum():
+    num_e = 5
+    num_f = 4
+
+    total = num_e * num_f
+    print("These are number multiplied: " + str(total) )
+multiNum()
+
+def divNum():
+    num_g = 20
+    num_h = 5
+
+    result = num_g/num_h
+    print("These are numbers divided: " + str(result))
+divNum()    
