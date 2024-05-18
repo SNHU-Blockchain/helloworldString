bca4e84e05833ecd138c30f8d031237b5b027ab5
ladeeluc <laura.hatch@snhu.edu>
2024-05-17 19:40:19 -0500

    conditional statment

---
+++ Blockchain.py/control_structures.py
@@ -0,0 +1,19 @@
+def oddNum():
+    num = 3 
+    if num % 2 == 0:
+        print("This number is even!")
+    else:
+        print("This number is odd!")
+
+oddNum()
+
+def evenNum():
+    numEven = 4
+    if numEven % 2 != 0:
+        print("This number is odd!")
+    else:
+        print("This number is even!")
+
+evenNum()    
+
+
